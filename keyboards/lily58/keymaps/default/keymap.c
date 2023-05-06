3134c021df0779855fe55ff4ea303384be6019bb
Michael Mead <mmead.developer@gmail.com>
2023-05-05 23:08:56 -0700

    chore(lily58): swap enter and space
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Home  |    | End   |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LOWER| RAISE| /BackSP /       \Space \  |Enter |RGUI  | RAlt |
+ *                   | LGUI | LOWER| RAISE| /BackSP /       \Enter \  |Space |RGUI  | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_Q,   KC_W,    KC_E,       KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_ESC,
   KC_LCTL,  KC_A,   KC_S,    KC_D,       KC_F,    KC_G,                   KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,       KC_V,    KC_B, KC_HOME,  KC_END, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                    KC_LGUI, MO(_LOWER), MO(_RAISE),    KC_BSPC,  KC_SPC, KC_ENT,  KC_RGUI, KC_RALT
+                    KC_LGUI, MO(_LOWER), MO(_RAISE),    KC_BSPC,  KC_ENT, KC_SPC,  KC_RGUI, KC_RALT
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Home  |    | End   |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |   -  |   +  |   [  |   ]  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LOWER| RAISE| /BackSP /       \Space \  |Enter |RGUI  | RAlt |
+ *                   | LGUI | LOWER| RAISE| /BackSP /       \Enter \  |Space |RGUI  | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -62,7 +62,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------| Home  |    | End   |--------+------+------+---------+------+------|
  * |  F7  |  F8  |  F9  | F10  | F11  | F12  |-------|    |-------|Opt+Left| PGDN | PGUP |Opt+Right|      |      |
  * `-----------------------------------------/       /     \      \----------------------------------------------'
- *                   | LGUI | LOWER| RAISE| /BackSP /       \Space \  |Enter |BackSP| RAlt |
+ *                   | LGUI | LOWER| RAISE| /BackSP /       \Enter \  |Space |BackSP| RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
@@ -84,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|       |    |       |------+------+------+------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|      |      | MODE | HUE- | SAT- | VAL- |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
- *                   | LGUI | LOWER| RAISE| /Space  /       \Space \  |Enter |BackSP| RAlt |
+ *                   | LGUI | LOWER| RAISE| /BackSP  /       \Enter \  |Space |RGUI  | RAlt |
  *                   |      |      |      |/       /         \      \ |      |      |      |
  *                   `----------------------------'           '------''--------------------'
  */
