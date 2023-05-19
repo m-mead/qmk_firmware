93466910afb6431a8f25d466b39950274598cc03
Michael Mead <mmead.developer@gmail.com>
2023-05-18 18:00:26 -0700

    feat(lily58): add tap/hold layer for backspace key
    Signed-off-by: Michael Mead <mmead.developer@gmail.com>

--- keyboards/lily58/keymaps/default/keymap.c
+++ keyboards/lily58/keymaps/default/keymap.c
@@ -18,8 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  |BackSp|
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * |C/Esc |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  C/' |
- * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
+ * |C/Esc |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  ' |
+ * |------+------+------+------+------+------| O/[   |    |  C/]   |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LOpt | LGui | Lower | /Space /        \Enter \  | Raise| RGui| ROpt |
@@ -28,11 +28,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 
  [_QWERTY] = LAYOUT(
-  KC_GRV,          KC_1,    KC_2,    KC_3,       KC_4,   KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
-  KC_TAB,          KC_Q,    KC_W,    KC_E,       KC_R,   KC_T,                         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
-  LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,       KC_F,   KC_G,                         KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  RCTL_T(KC_QUOT),
-  KC_LSFT,         KC_Z,    KC_X,    KC_C,       KC_V,   KC_B,    KC_LBRC,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-                   KC_LOPT, KC_LGUI, MO(_LOWER), KC_SPC, KC_ENT,  MO(_RAISE), KC_RGUI, KC_ROPT
+  KC_GRV,          KC_1,    KC_2,    KC_3,                    KC_4,   KC_5,                                    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_MINS,
+  KC_TAB,          KC_Q,    KC_W,    KC_E,                    KC_R,   KC_T,                                    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_BSPC,
+  LCTL_T(KC_ESC),  KC_A,    KC_S,    KC_D,                    KC_F,   KC_G,                                    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+  KC_LSFT,         KC_Z,    KC_X,    KC_C,                    KC_V,   KC_B, LOPT_T(KC_LBRC),  RCTL_T(KC_RBRC), KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+                   KC_LOPT, KC_LGUI, LT(_LOWER, KC_BSPC),     KC_SPC,       KC_ENT,           MO(_RAISE),       KC_RGUI,         KC_ROPT
 ),
 /* Raise
  * ,-----------------------------------------.                    ,--------------------------------------------.
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |      |      |      |      |      |                    | Home   |  End | PgUp | PgDn  |      |      |
  * |------+------+------+------+------+------|                    |--------+------+------+-------+------+------|
  * |      |      |      |      |      |      |-------.    ,-------|  Left  | Down |  Up  | Right |      |      |
- * |------+------+------+------+------+------|   [   |    |   ]   |--------+------+------+-------+------+------|
+ * |------+------+------+------+------+------|  C/[  |    | C/]   |--------+------+------+-------+------+------|
  * |      |      |      |      |      |      |-------|    |-------|        |      |      |       |      |      |
  * `-----------------------------------------/       /     \      \--------------------------------------------'
  *                   | LOpt | LGui | Lower| /Space  /       \Enter \  | Raise | RGui| ROpt |
@@ -63,7 +63,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |      |  F12 |      |      |      | Caps |                    |   ~  |   =  |   _  |   {  |   }  |      |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * |      |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |      |
- * |------+------+------+------+------+------|   [   |    |   ]   |------+------+------+------+------+------|
+ * |------+------+------+------+------+------| O/[   |    | C/]   |------+------+------+------+------+------|
  * |      |      |      |      |      |  \   |-------|    |-------|   |  |   -  |   +  |   [  |   ]  |      |
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *                   | LOpt | LGui| Lower | /Space /        \Enter \   |Raise |RGui | ROpt |
